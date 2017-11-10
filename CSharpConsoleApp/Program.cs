fb296136bfc6341f2885b963a54dff5191ae4e50
KuYoungMo <ku0mo@naver.com>
2017-11-10 21:04:49 +0900

    ToString() - 문자열 변환

--- CSharpConsoleApp/Program.cs
+++ CSharpConsoleApp/Program.cs
@@ -1,22 +1,61 @@
-﻿// 정수 및 실수 입력
-using System;
+﻿using System;
 namespace CSharpConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("실수 입력 : ");
-            double n1 = double.Parse(Console.ReadLine());
+            int n = 100;
+            double d = 5.5;
 
-            Console.Write("실수 입력 : ");
-            double n2 = double.Parse(Console.ReadLine());
-
-            double sum = n1 + n2;
-            Console.WriteLine("{0} + {1} = {2}", n1, n2, sum);
+            Console.WriteLine(n.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(100.ToString());
+            Console.WriteLine((5.5).ToString());
+            //ToString() - virtual method은 문자열로 바꾸어주는 역할
         }
     }
 }
+
+
+
+//using System;
+//namespace CSharpConsoleApp
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = 100;
+//            double d = 5.5;
+
+//            Console.WriteLine(n);
+//            Console.WriteLine(d);
+//        }
+//    }
+//}
+
+
+
+//// 정수 및 실수 입력
+//using System;
+//namespace CSharpConsoleApp
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.Write("실수 입력 : ");
+//            double n1 = double.Parse(Console.ReadLine());
+
+//            Console.Write("실수 입력 : ");
+//            double n2 = double.Parse(Console.ReadLine());
+
+//            double sum = n1 + n2;
+//            Console.WriteLine("{0} + {1} = {2}", n1, n2, sum);
+//        }
+//    }
+//}
 
 //// 문자 입력
 //using System;
