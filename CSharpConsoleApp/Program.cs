2a0f729b5d2c8c1dfd138473576e42cb47857dd3
KuYoungMo <ku0mo@naver.com>
2017-11-10 20:41:45 +0900

    문자열 입력받기

--- CSharpConsoleApp/Program.cs
+++ CSharpConsoleApp/Program.cs
@@ -1,22 +1,69 @@
-﻿namespace CSharpConsoleApp
+﻿// 문자열 입력
+using System; 
+namespace CSharpConsoleApp
 {
     class Program
     {
-        void Print()
-        {
-            System.Console.WriteLine("Hello!");
-            System.Console.WriteLine(100);
-            System.Console.WriteLine(3.55);
-            System.Console.WriteLine('A');
-        }
         static void Main(string[] args)
         {
-            Program pro = new Program(); // 프로그램 객체를 생성하여 호출해야 한다.
-                                         // 모든 객체는 new를 사용해서 호출!  value object , reference object 두개 다 사용할 수 있다.
-            pro.Print();
+            Console.Write("문자열 입력: ");
+            string s = Console.ReadLine(); // C#에서 모든 입력은 이 명령어를 사용한다.
+            Console.WriteLine("string : {0}", s);
         }
     }
 }
+
+
+//using System;
+//namespace CSharpConsoleApp
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.Write(100);
+//            Console.Write('A');
+//            Console.Write("ABCD\n");
+//            Console.Write("abcd\n");
+//        }
+//    }
+//}
+
+
+//using System;
+//namespace CSharpConsoleApp
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello!");
+//            Console.WriteLine("{0}, {1}", "ABC", 100);
+//            Console.WriteLine("{0}, {1}, {0}", "ABC", 100);
+//        }
+//    }
+//}
+
+
+//namespace CSharpConsoleApp
+//{
+//    class Program
+//    {
+//        static void Print() // 객체 것이 아닌 독립적으로 존재하는 클래스의 것(static)
+//        {
+//            System.Console.WriteLine("Hello!");
+//            System.Console.WriteLine(100);
+//            System.Console.WriteLine(3.55);
+//            System.Console.WriteLine('A');
+//        }
+//        static void Main(string[] args) //main 함수는 static이어야 한다.
+//        {
+//            Print(); // 프로그램이라는 클래스 안에 있어서 사용 가능
+//            Program.Print();
+//            CSharpConsoleApp.Program.Print(); // 정식 풀 Name
+//        }
+//    }
+//}
 
 
 
