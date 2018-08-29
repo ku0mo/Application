be37cc438dd008feac579a5cb8f10794bb3f9fd8
영모 구 <ymo4303@gmail.com>
2018-08-29 11:12:01 +0900

    상속, 다형성

--- CSharpConsoleApp/Program.cs
+++ CSharpConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// Equals, ReferenceEquals
+﻿// 상속, 다형성
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,55 +7,145 @@ using System.Text;
 
 namespace CSharpConsoleApp
 {
-    class Point
+    abstract class Shape
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public Point(int _x, int _y)
+        public Shape(string name = "NoName")
+        { PetName = name; }
+
+        // A single abstract method.
+        public abstract void Draw();
+
+        public string PetName { get; set; }
+    }
+
+    #region Circle class
+    // If we did not implement the abstract Draw() method, Circle would also be
+    // considered abstract, and would have to be marked abstract!
+    class Circle : Shape
+    {
+        public Circle() { }
+        public Circle(string name) : base(name) { }
+        public override void Draw()
         {
-            X = _x;
-            Y = _y;
-        }
-        public override string ToString()
-        {
-            return X + "," + Y;
-        }
-        public override bool Equals(object obj)
-        {
-            Point pt = obj as Point;
-            return this.X == pt.X && this.Y == pt.Y;
-        }
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
+            Console.WriteLine("Drawing {0} the Circle", PetName);
         }
     }
+    #endregion
+
+    #region Hexagon class
+    // Hexagon DOES override Draw().
+    class Hexagon : Shape
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Hexagon", PetName);
+        }
+    }
+    #endregion
+
+    #region ThreeDCircle class
+    // This class extends Circle and hides the inherited Draw() method.
+    class ThreeDCircle : Circle
+    {
+        // Hide any Draw() implementation above me.
+        public new void Draw()
+        {
+            Console.WriteLine("Drawing a 3D Circle");
+        }
+        public new string PetName { get; set; }
+    }
+    #endregion
     class Program
     {
         static void Main(string[] args)
         {
-            Point pt1 = new Point(2, 3);
-            Point pt2 = new Point(2, 3);
+            Console.WriteLine("***** Fun with Polymorphism *****\n");
 
-            Console.WriteLine("{0}, {1}", pt1, pt2);
+            // Make an array of Shape-compatible objects.
+            Shape[] myShapes = {new Hexagon(), new Circle(), new Hexagon("Mick"),
+                                new Circle("Beth"), new Hexagon("Linda")};
 
-            if (pt1.Equals(pt1))
-                Console.WriteLine("true");
-            else
-                Console.WriteLine("false");
+            // Loop over each item and interact with the
+            // polymorphic interface.
+            foreach (Shape s in myShapes)
+            {
+                s.Draw();
+            }
 
-            if (Point.Equals(pt1, pt2))
-                Console.WriteLine("true");
-            else
-                Console.WriteLine("false");
+            // This calls the Draw() method of the ThreeDCircle.
+            ThreeDCircle o = new ThreeDCircle();
+            o.Draw();
 
-            if (Point.ReferenceEquals(pt1, pt2))
-                Console.WriteLine("true");
-            else
-                Console.WriteLine("false");
+            // This calls the Draw() method of the parent!
+            ((Circle)o).Draw();
+            Console.ReadLine();
         }
     }
 }
+
+
+
+
+//// Equals, ReferenceEquals
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace CSharpConsoleApp
+//{
+//    class Point
+//    {
+//        public int X { get; set; }
+//        public int Y { get; set; }
+//        public Point(int _x, int _y)
+//        {
+//            X = _x;
+//            Y = _y;
+//        }
+//        public override string ToString()
+//        {
+//            return X + "," + Y;
+//        }
+//        public override bool Equals(object obj)
+//        {
+//            Point pt = obj as Point;
+//            return this.X == pt.X && this.Y == pt.Y;
+//        }
+//        public override int GetHashCode()
+//        {
+//            return base.GetHashCode();
+//        }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Point pt1 = new Point(2, 3);
+//            Point pt2 = new Point(2, 3);
+
+//            Console.WriteLine("{0}, {1}", pt1, pt2);
+
+//            if (pt1.Equals(pt1))
+//                Console.WriteLine("true");
+//            else
+//                Console.WriteLine("false");
+
+//            if (Point.Equals(pt1, pt2))
+//                Console.WriteLine("true");
+//            else
+//                Console.WriteLine("false");
+
+//            if (Point.ReferenceEquals(pt1, pt2))
+//                Console.WriteLine("true");
+//            else
+//                Console.WriteLine("false");
+//        }
+//    }
+//}
 
 
 //// Equals, ReferenceEquals
